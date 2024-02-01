4561855e3591a12e7a2f28b614c3c949d3436480
ssw1992 <ssw1992@qq.com>
2024-02-01 11:46:43 +0800

    feat: first

---
+++ README.md
@@ -0,0 +1,20 @@
+Api-generator
+======
+
+借助Api-generator，你可以在命令行把swagger的json信息生成开发中使用的api文件
+
+### 安装
+
+```cmd
+npm i api-generator -g
+```
+
+### 使用
+
+```cmd
+api-generator -u [swagger json的url 或者 本地地址] -o [输入文件夹 默认 ./src/api]
+```
+
+### 未来计划实现
+#### 自定义模板
+#### 输出ts类型
---
+++ bin/main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import { Command } from "commander";
+
+import load from "../lib/load.js";
+import parser from "../lib/parser.js";
+import generate from "../lib/generate.js";
+import fs from "fs";
+import path from "path";
+import { readFile } from "../lib/file.js";
+const program = new Command();
+
+program
+  .description("解析swagger json 信息生成 api")
+  .requiredOption("-u, --url <http|string>", "获取 swagger.json 的 url")
+  .option("-o, --outPath <string>", "api 生成路径", "./src/api")
+  // .option('-t, --templatePath <string>', '使用指定的 swagger 模板路径')
+  .action(async ({ url, outPath }) => {
+    const swaggerJson = await load(url);
+    const apiGroups = await parser(swaggerJson);
+    const templateData = await readFile("api.hbs");
+
+    outPath = outPath.endsWith("/") ? outPath : outPath + "/";
+    if (!fs.existsSync(outPath)) {
+      fs.mkdirSync(outPath, { recursive: true });
+    }
+    await Promise.all(
+      apiGroups.map((params) =>
+        generate({
+          templateData,
+          outPath,
+          params,
+        })
+      )
+    );
+    console.log("end");
+  });
+
+program.parse();
